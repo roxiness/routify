54670ae5abf6236a24411b091338c443225744c8
Jakob Rosenberg <jakobrosenberg@gmail.com>
2019-10-10 11:25:07 +0200

    added url helper

--- src/components.js
+++ src/components.js
@@ -1,8 +1,16 @@
+import { get } from 'svelte/store'
 export { default as Router } from './Router.svelte';
 export { default as Route } from './Route.svelte';
 export { route } from './store'
+import { route } from './store'
 
-export const href = (path, params) => {
+
+export const url = (path, params) => {
+
+    if (!path.match(/^\//)) {
+        path = get(route).url.replace(/\/index$/, '/') + path
+    }
+    params = Object.assign(get(route), params)
     for (let [key, value] of Object.entries(params)) {
         path = path.replace(`:${key}`, value)
     }
