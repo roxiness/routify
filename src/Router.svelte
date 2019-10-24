8a131d818ad61c1ac6a675ec6d0c13164fb19cd9
Jakob Rosenberg <jakobrosenberg@gmail.com>
2019-10-24 09:50:25 +0200

    allow Router to accept scoped variables

--- src/Router.svelte
+++ src/Router.svelte
@@ -3,9 +3,10 @@
   import Route from "./Route.svelte";
   import init from "./navigator.js";
   import { routes } from "generatedRoutes.js";
+  export let scoped
 
   let components, route;
   init(routes, update => ({ components, route } = update));
 </script>
 
-<Route {components} {route} {routes} {url} />
+<Route {components} {route} {routes} {url} rootScope={scoped} />
