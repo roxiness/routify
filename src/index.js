2e9524fd501ea090fcb05ebf2cca1ee438c58547
rixo <rixo@rixo.fr>
2019-11-22 10:26:57 +0100

    refactor: separate node code from client side code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-module.exports.fileRouter = require('./rollup-plugin-filerouter')
+export { default as Router } from './Router.svelte';
+export { default as Route } from './Route.svelte';
+export * from './store'
+export * from './utils'
