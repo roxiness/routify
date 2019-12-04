1aa30a90a8de3464f39e603afbc8a30a06e97bf0
Jakob Rosenberg <jakobrosenberg@gmail.com>
2019-12-04 09:13:10 +0100

    changed src to runtime

--- runtime/Route.svelte
+++ runtime/Route.svelte
@@ -1,27 +1,39 @@
 <script>
+  import { setContext, getContext } from "svelte";
   import { demandObject, suppressWarnings } from "./scripts.js";
-  
+
   export let url, route, routes;
   export let components = [];
   export let rootScope = {};
   export let layoutScope = {};
   export let _routeOptions = {};
 
+  $:console.log($$props)
+
   $: rootScope = Object.assign({}, rootScope, layoutScope);
 
-  $: [getComponent, ...remainingComponents] = components
+  $: [getComponent, ...remainingComponents] = components;
 
-  $: routerProps = {url, route, routes, _routeOptions}
+  $: routerProps = { url, route, routes, _routeOptions };
 
-  $: props = Object.assign({}, routerProps, {scoped: Object.assign({}, rootScope )}, rootScope );
-  
-  $: if (!_routeOptions.unknownPropWarnings) suppressWarnings(Object.keys(props));
+  $: props = Object.assign(
+    {},
+    routerProps,
+    { scoped: Object.assign({}, rootScope) },
+    rootScope
+  );
+
+  $: if (!_routeOptions.unknownPropWarnings)
+    suppressWarnings(Object.keys(props));
 </script>
 
 {#await getComponent() then cmp}
   <svelte:component this={cmp} {...props} let:scoped={layoutScope}>
     {#if remainingComponents.length && demandObject(layoutScope)}
-      <svelte:self {...routerProps} components={remainingComponents} {layoutScope} />
+      <svelte:self
+        {...routerProps}
+        components={remainingComponents}
+        {layoutScope} />
     {/if}
   </svelte:component>
 {/await}
