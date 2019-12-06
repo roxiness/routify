b62506fa105ade76f3b073b337c77796fcb48d96
Jakob Rosenberg <jakobrosenberg@gmail.com>
2019-12-06 12:24:56 +0100

    feed routes manually

--- runtime/Router.svelte
+++ runtime/Router.svelte
@@ -1,12 +1,9 @@
 <script>
   import { setContext } from "svelte";
-  import { url } from "./utils";
   import Route from "./Route.svelte";
   import init from "./navigator.js";
-  import { routes as defaultRoutes } from "../tmp/routes.js";
+  
   export let routes;
-
-  routes = routes || defaultRoutes;
 
   setContext("routify", {
     routes,
