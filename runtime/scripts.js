573feb493e4105e072979d478bc979fbcc8a0314
rixo <rixo@rixo.fr>
2019-12-06 17:37:55 +0100

    run prettier on the project

--- runtime/scripts.js
+++ runtime/scripts.js
@@ -1,32 +1,29 @@
+export const suppressWarnings = (function() {
+  const _ignoreList = []
+  let initialized = false
+  const _warn = console.warn
 
-export const suppressWarnings = (function () {
-    const _ignoreList = []
-    let initialized = false
-    const _warn = console.warn
+  return function(newIgnores = []) {
+    newIgnores.forEach(key => {
+      if (!_ignoreList.includes(key)) _ignoreList.push(key)
+    })
 
-    return function (newIgnores = []) {
-        newIgnores.forEach(key => {
-            if (!_ignoreList.includes(key))
-                _ignoreList.push(key)
-        })
-
-        if (!initialized) {
-            initialized = true
-            console.warn = function (...params) {
-                const msg = params[0]
-                const match = _ignoreList.filter(prop => msg.match(new RegExp(`was created with unknown prop '${prop}'`))).length
-                if (!match)
-                    _warn(...params)
-            }
-        }
+    if (!initialized) {
+      initialized = true
+      console.warn = function(...params) {
+        const msg = params[0]
+        const match = _ignoreList.filter(prop =>
+          msg.match(new RegExp(`was created with unknown prop '${prop}'`))
+        ).length
+        if (!match) _warn(...params)
+      }
     }
+  }
 })()
 
-export const demandObject = function (obj) {
-    const isObj = Object.prototype.toString.call(obj) === "[object Object]";
-    if (isObj || !obj) return true;
-    else
-        throw new Error(
-            `"${obj}" is not an object. "scoped" prop must an object`
-        );
+export const demandObject = function(obj) {
+  const isObj = Object.prototype.toString.call(obj) === '[object Object]'
+  if (isObj || !obj) return true
+  else
+    throw new Error(`"${obj}" is not an object. "scoped" prop must an object`)
 }
