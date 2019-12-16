b79a990bf3b0e7ab6b5cf5407d585def4e11553a
Jakob Rosenberg <jakobrosenberg@gmail.com>
2019-12-16 19:49:16 +0100

    remove "fake" root

--- runtime/Router.svelte
+++ runtime/Router.svelte
@@ -1,15 +1,9 @@
 <script>
   import { writable } from 'svelte/store'
-import { setContext } from 'svelte'
   import Route from './Route.svelte'
   import init from './navigator.js'
 
   export let routes
-
-  // setContext('routify', writable({
-  //   routes,
-  //   route: 'Root'
-  // }));
 
   let layouts, route
   init(routes, update => ({ layouts, route } = update))
