58a921e007fda43b21b317a4f32762d06c4327e3
rixo <rixo@rixo.fr>
2019-12-29 23:52:38 +0100

    add last version of @rixo/eslint-config-svelte without broken postinstall

--- .eslintrc.js
+++
@@ -1,5 +0,0 @@
-// needed for now as a limitation of @rixo/eslint-config-svelte
-// (otherwise a broken stub will be recreated on npm install something)
-module.exports = {
-  extends: './.eslintrc',
-}
