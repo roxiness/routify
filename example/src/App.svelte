f28a641e9775a7701271e45601f606bb5ad35343
rixo <rixo@rixo.fr>
2019-12-30 02:35:48 +0100

    upgrade to last test-hmr and refactor test dir layout

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import { Router } from '@sveltech/routify'
-  import { routes } from '@sveltech/routify/tmp/routes'
+  import { routes } from './.tmp/routes.js'
 </script>
 
 <Router {routes} />
--- test/e2e/navigation-by-url.spec.js
+++ test/e2e/navigation-by-url.spec.js
@@ -2,7 +2,7 @@ import thc from 'test-hmr'
 import { goto, expectPageLoad } from 'test-hmr/helpers'
 
 describe('navigation by url', () => {
-  thc.only`
+  thc`
     # reloads the page, & does not 404
 
     --- pages/index.svelte ---
