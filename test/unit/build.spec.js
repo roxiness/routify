522fef4a90e34a4eef00b6fe14e6baace2fcabc4
rixo <rixo@rixo.fr>
2020-03-02 02:10:53 +0100

    untrash tests: replace pta with zorax & zoar, replace DI with rewiremock

---
+++ test/unit/build.spec.js
@@ -0,0 +1,71 @@
+import { test } from '@@'
+import { Volume } from 'memfs'
+import * as path from 'path'
+import * as fs from 'fs'
+import rewiremock from 'rewiremock'
+
+import fsa from '../../lib/utils/fsa'
+// import { Builder } from '../../lib/services/interface'
+
+const resolveSample = (...names) => path.resolve(__dirname, 'build', ...names)
+
+const files = fs.readdirSync(resolveSample())
+
+const inputRe = /^(.+)\.input\.js$/
+
+const runSample = async (t, file, name) => {
+  const { default: spec } = await import(resolveSample(file))
+
+  const vol = Volume.fromJSON(spec.files)
+
+  const defaultOptions = {
+    extensions: '.svelte',
+  }
+
+  const { Builder } = await rewiremock.module(
+    () => import('../../lib/services/interface'),
+    {
+      '../utils/fsa': fsa.from(vol),
+    }
+  )
+
+  const build = Builder(
+    {
+      ...defaultOptions,
+      ...spec.options,
+    },
+    true
+  )
+
+  const actual = await build(true)
+
+  const expectedFilename = resolveSample(`${name}.expected.js`)
+  const hasExpected = await fsa.exists(expectedFilename)
+
+  if (!hasExpected) {
+    await fsa.writeFile(expectedFilename, actual, 'utf8')
+  }
+
+  // fail if expected file was not present
+  t.ok(hasExpected, `${name}.expected.js exists`)
+
+  const expected = await fsa.readFile(expectedFilename, 'utf8')
+
+  const stripTimestamp = code =>
+    code.replace(/File generated.*/, 'File generated __TIMESTAMP__')
+
+  t.eq(
+    stripTimestamp(actual),
+    stripTimestamp(expected),
+    'content is as expected'
+  )
+}
+
+runSample.title = (title, file, name) => title || name
+
+files
+  .map(file => inputRe.exec(file))
+  .filter(Boolean)
+  .forEach(([file, name]) => {
+    test(runSample, file, name)
+  })
---
+++ test/unit/sanity.spec.js
@@ -0,0 +1,11 @@
+import { test } from '@@'
+
+test('the test framework', t => {
+  t.test('works', t => {
+    t.ok(true, 'indeed')
+  })
+
+  t.test('multiple times', t => {
+    t.ok(true, 'two')
+  })
+})
