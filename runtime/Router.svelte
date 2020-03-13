fbfa807cdddd35ded13eace3798a8bd8c136c4b8
rixo <rixo@rixo.fr>
2020-03-13 21:29:08 +0100

    streamline Router workflow

--- runtime/Router.svelte
+++ runtime/Router.svelte
@@ -5,25 +5,41 @@
   import { routes as routesStore } from './store.js'
   import { suppressWarnings } from './utils.js'
 
+  export let routes
+
+  let layouts
+  let navigator
+
   suppressWarnings()
 
   if (!window.routify) {
     window.routify = {}
   }
 
-  export let routes
-  routesStore.set(routes)
-  let layouts = []
+  const updatePage = (...args) => navigator && navigator.updatePage(...args)
+
+  setContext('routifyupdatepage', updatePage)
 
   const callback = res => (layouts = res)
 
-  $: ({ updatePage, destroy } = init($routesStore, callback))
-  $: updatePage()
-  $: setContext('routifyupdatepage', updatePage)
+  const cleanup = () => {
+    if (!navigator) return
+    navigator.destroy()
+    navigator = null
+  }
 
-  onDestroy(() => {
-    destroy()
-  })
+  const doInit = () => {
+    cleanup()
+    navigator = init(routes, callback)
+    routesStore.set(routes)
+    navigator.updatePage()
+  }
+
+  $: if (routes) doInit()
+
+  onDestroy(cleanup)
 </script>
 
-<Route {layouts} />
+{#if layouts}
+  <Route {layouts} />
+{/if}
