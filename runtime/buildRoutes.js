f4187bd583aad1dd1a33b50cdb6351d266231442
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-22 20:14:19 +0100

    feat(exports): add tree for use in sitemaps

--- runtime/buildRoutes.js
+++ runtime/buildRoutes.js
@@ -1,18 +1,21 @@
 import { pathToParams, pathToRank, pathToRegex } from './utils'
 
-export function buildRoutes(routes, routeKeys) {
+export function buildRoutes(tree) {
+  const treeWithLayouts = applyLayouts(tree)
+  const routes = flattenTree(treeWithLayouts)
   return (
     routes
-      // .map(sr => deserializeRoute(sr, routeKeys))
+      .filter(route => !route.isLayout)
       .map(decorateRoute)
       .sort((c, p) => (c.ranking >= p.ranking ? -1 : 1))
   )
 }
 
-const decorateRoute = function(route) {
+const decorateRoute = function (route) {
   route.paramKeys = pathToParams(route.path)
   route.regex = pathToRegex(route.path, route.isFallback)
   route.name = route.path.match(/[^\/]*\/[^\/]+$/)[0].replace(/[^\w\/]/g, '') //last dir and name, then replace all but \w and /
+  route.shortPath = route.path.replace(/\/(index|_fallback)$/, '')
   route.ranking = pathToRank(route)
   route.layouts.map(l => {
     l.param = {}
@@ -23,10 +26,35 @@ const decorateRoute = function(route) {
   return route
 }
 
-export function deserializeRoute(route, keys) {
-  const obj = {}
-  route.forEach((v, i) => {
-    obj[keys[i]] = v
+/**
+ * applyLayouts
+ * @param {Object} tree
+ * @param {Array} layouts
+ */
+function applyLayouts(tree, layouts = []) {
+  return tree.map(file => {
+    if (file.dir) {
+      file.dir = applyLayouts(file.dir, [...layouts])
+    } else {
+      if (file.isReset || file.meta.$reset) layouts = []
+      if (file.isLayout) layouts.push(file)
+      else file.layouts = layouts
+    }
+    return file
   })
-  return obj
+}
+
+/**
+ * flattenTree
+ * @param {Object} tree
+ * @param {Array} arr
+ */
+function flattenTree(tree, arr = []) {
+  tree.forEach(file => {
+    if (file.dir)
+      arr.push(...flattenTree(file.dir))
+    else
+      arr.push(file)
+  })
+  return arr
 }
