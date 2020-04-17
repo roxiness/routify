8f563e5da10c885b2f799123593510529daf39c0
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-04-17 11:35:14 +0200

    improve tests

--- test/e2e/helpers/run.js
+++ test/e2e/helpers/run.js
@@ -1,13 +1,15 @@
-const { getBranch, exec, spawn, log } = require('./shared')
+const { getBranch, exec, log } = require('./shared')
 const path = require('path')
 
-async function run({ branch, buildsDir, projectDir }) {
+async function run({ branch, buildsDir, projectDir, args, app }) {
     branch = branch || await getBranch(projectDir)
     const buildDir = path.resolve(buildsDir, `starter-template#${branch}`)
 
-    exec(`testcafe chrome starter-example.js`
-        + ` --app "npm run dev --prefix ${buildDir}"`,
-        { cwd: path.resolve(__dirname, '../tests') })
+    app = app ? `--app "${app.replace('__buildDir__', buildDir)}"` : ''
+
+    const argsStr = args.join(' ')
+     exec(`testcafe chrome starter-example.js ${app} ${argsStr}`,
+         { cwd: path.resolve(__dirname, '../tests') })
 }
 
 module.exports = run
--- test/e2e/index.js
+++ test/e2e/index.js
@@ -5,7 +5,7 @@ const setup = require('./helpers/setup')
 const run = require('./helpers/run')
 const projectDir = path.resolve(__dirname, '../..')
 const buildsDir = path.resolve(__dirname, 'builds')
-
+console.log('hello')
 
 program
     .command('setup')
@@ -14,13 +14,18 @@ program
     .action(async program => {
         const { purge, branch } = program.opts()
         setup({ projectDir, buildsDir, purge, branch })
-    })
+    }).parseOptions(process.argv)
 
 program.command('test')
     .option('-b --branch [branch]', 'branch to use', "")
-    .action(async program => {
-        const { branch } = program.opts()
-        run({ branch, buildsDir, projectDir })
+    .option('-a --app [command]', 'app to start', "")
+    .allowUnknownOption()
+    .action(async _program => {
+        const { branch, app, } = _program.opts()
+        const { args } = _program        
+        run({ branch, app, buildsDir, projectDir, args })
     })
+    .parseOptions(process.argv)
+
 
 program.parse(process.argv)
