3c10ce24bb9fd38c470611f42539f8ea74ec0ff5
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-04-19 17:53:21 +0200

    fix(basepath): show warning if / is missing

--- runtime/store.js
+++ runtime/store.js
@@ -1,4 +1,5 @@
 import { writable, derived } from 'svelte/store'
+import '../typedef'
 
 /** @type {import('svelte/store').Writable<RouteNode>} */
 export const route = writable(null) // the actual route being rendered
@@ -6,7 +7,24 @@ export const routes = writable([]) // all routes
 
 /** @type {import('svelte/store').Writable<RouteNode>} */
 export const urlRoute = writable(null)  // the route matching the url
-export const basepath = writable("") // basepath regex
+
+/** 
+ * @typedef {import('svelte/store').Writable<String>} Basepath
+ * @type {Basepath} */
+export const basepath = (() => {
+    const { set, subscribe } = writable("")
+
+    return {
+        subscribe,
+        set(value) {
+            if (value.match(/^\//))
+                set(value)
+            else console.warn('Basepaths must start with /')
+        },
+        update() { console.warn('Use assignment or set to update basepaths.') }
+    }
+})()
+
 export const location = derived( // the part of the url matching the basepath
     [basepath, urlRoute],
     ([$basepath, $route]) => {
