2de1346fc0fd201e72a62a429d69c3696aa46cd9
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-04-26 12:36:26 +0200

    fix(types): some types were missing

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,20 @@
 /// <reference path="./typedef.js" />
 
-declare module '@sveltech/routify' {    
-    export const Router: import('svelte/internal').SvelteComponent    
-    export const url: import('./runtime/helpers.js').UrlHelperStore
-    export const goto: import('./runtime/helpers.js').GotoHelperStore
-    export const params: import('./runtime/helpers.js').ParamsHelperStore
-    export const isActive: import('./runtime/helpers.js').IsActiveHelperStore
-    export const leftover: import('./runtime/helpers.js').LeftoverHelperStore
-    export const beforeUrlChange: import('./runtime/helpers.js').BeforeUrlChangeHelperStore
-    export const ready: import('./runtime/helpers.js').ReadyHelperStore
-    export const focus: import('./runtime/helpers.js').FocusHelper
-    export const getConcestor: import('./runtime/helpers.js').GetConcestor
-    export const basepath: import('./runtime/store').Basepath
-    export const page: import('./runtime/helpers.js').ClientNodeHelperStore
-    export const layout: import('./runtime/helpers.js').ClientNodeHelperStore
+declare module '@sveltech/routify' {  
+    export * from './types/runtime/helpers'
+    export * from './types/runtime/store'
+    export const Router: import('svelte/internal').SvelteComponent
     
     global {
         export const $page:ClientNodeApi
         export const $layout:ClientNodeApi
-        export const $params:import('./runtime/helpers.js').ParamsHelper
-        export const $leftover:import('./runtime/helpers.js').LeftoverHelper
-        export const $url: import('./runtime/helpers.js').UrlHelper
-        export const $isActive: import('./runtime/helpers.js').IsActiveHelper
-        export const $goto: import('./runtime/helpers.js').GotoHelper
-        export const $beforeUrlChange: import('./runtime/helpers.js').BeforeUrlChangeHelper
-        export const $ready: import('./runtime/helpers.js').ReadyHelper
+        export const $params:import('./types/runtime/helpers').ParamsHelper
+        export const $leftover:import('./types/runtime/helpers').LeftoverHelper
+        export const $url: import('./types/runtime/helpers').UrlHelper
+        export const $isActive: import('./types/runtime/helpers').IsActiveHelper
+        export const $goto: import('./types/runtime/helpers').GotoHelper
+        export const $beforeUrlChange: import('./types/runtime/helpers').BeforeUrlChangeHelper
+        export const $ready: import('./types/runtime/helpers').ReadyHelper
         export let $basepath: String
     }
 }
