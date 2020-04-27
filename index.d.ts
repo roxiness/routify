e92ea0cca5b33a2e6b19cbf87873e766147ba066
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-04-27 12:24:58 +0200

    fix(types): ambient had relative exports

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,20 @@
 /// <reference path="./typedef.js" />
 
 declare module '@sveltech/routify' {  
-    export * from './types/runtime/helpers'
-    export * from './types/runtime/store'
+    export * from '@sveltech/routify/runtime/helpers'
+    export * from '@sveltech/routify/runtime/store'
     export const Router: import('svelte/internal').SvelteComponent
     
     global {
         export const $page:ClientNodeApi
         export const $layout:ClientNodeApi
-        export const $params:import('./types/runtime/helpers').ParamsHelper
-        export const $leftover:import('./types/runtime/helpers').LeftoverHelper
-        export const $url: import('./types/runtime/helpers').UrlHelper
-        export const $isActive: import('./types/runtime/helpers').IsActiveHelper
-        export const $goto: import('./types/runtime/helpers').GotoHelper
-        export const $beforeUrlChange: import('./types/runtime/helpers').BeforeUrlChangeHelper
-        export const $ready: import('./types/runtime/helpers').ReadyHelper
+        export const $params:import('./runtime/helpers').ParamsHelper
+        export const $leftover:import('./runtime/helpers').LeftoverHelper
+        export const $url: import('./runtime/helpers').UrlHelper
+        export const $isActive: import('./runtime/helpers').IsActiveHelper
+        export const $goto: import('./runtime/helpers').GotoHelper
+        export const $beforeUrlChange: import('./runtime/helpers').BeforeUrlChangeHelper
+        export const $ready: import('./runtime/helpers').ReadyHelper
         export let $basepath: String
     }
 }
