a94c128e6839ca10060a0b202b1dcb5c56609d10
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-05-28 15:30:51 +0200

    add generic route to mock api

--- example2/api-server/index.js
+++ example2/api-server/index.js
@@ -5,11 +5,24 @@ app.use(require('body-parser').json());
 app.use(cors())
 const port = 5432
 
+
+app.get('/generic', (req, res) => {
+    const delay = req.headers['x-delay'] || 0
+    const statusCode = req.headers['x-status-code']
+    if (statusCode) res.status(statusCode)
+    setTimeout(() => {
+        res.send({
+            date: Date.now().toString(),
+            ...req.query
+        })
+    }, delay);
+})
+
 app.get('/date', (req, res) => {
     const delay = req.headers['x-delay'] || 0
     const statusCode = req.headers['x-status-code']
     if (statusCode) res.status(statusCode)
-    setTimeout(() => { res.send(Date.now().toString()) }, delay);    
+    setTimeout(() => { res.send(Date.now().toString()) }, delay);
 })
 
 app.get('/echo', (req, res) => {
