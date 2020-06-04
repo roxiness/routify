30993c015856217baf6f6f1a4f4a825710cf5446
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-06-04 16:48:29 +0200

    allow example app to accept ?basepath=<basepath>

--- example2/src/App.svelte
+++ example2/src/App.svelte
@@ -1,9 +1,13 @@
 <script>
   import { setContext } from 'svelte'
-  import { Router } from '@sveltech/routify'
+  import { Router, basepath } from '@sveltech/routify'
   import { routes } from '@sveltech/routify/tmp/routes'
   import { writable } from 'svelte/store'
   import ServiceWorker from './ServiceWorker.svelte'
+
+  const params = new URLSearchParams(location.search)
+  const bp = params.get('basepath')
+  if (bp) $basepath = bp    
 </script>
 
 <Router {routes} />
