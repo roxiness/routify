38b352604146c23fb7dbb25d0f43afe3e00c8c5b
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-10-09 18:38:58 +0200

    fix: typings for hmr

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,30 @@
 /// <reference path="./typedef.js" />
 
-declare module '@roxi/routify' {  
-    export * from '@roxi/routify/typings/runtime/helpers'
-    export * from '@roxi/routify/typings/runtime/store'
-    export const Router: import('svelte/internal').SvelteComponent
-    export const routify: typeof import('@roxi/routify/plugins/rollup')
-    
-    global {
-        export const $page:ClientNodeApi
-        export const $layout:ClientNodeApi
-        export const $params:import('./typings/runtime/helpers').ParamsHelper
-        export const $leftover:import('./typings/runtime/helpers').LeftoverHelper
-        export const $url: import('./typings/runtime/helpers').UrlHelper
-        export const $isActive: import('./typings/runtime/helpers').IsActiveHelper
-        export const $goto: import('./typings/runtime/helpers').GotoHelper
-        export const $beforeUrlChange: import('./typings/runtime/helpers').BeforeUrlChangeHelper
-        export const $ready: import('./typings/runtime/helpers').ReadyHelper
-    }
+declare module '@roxi/routify' {
+  export * from '@roxi/routify/typings/runtime/helpers'
+  export * from '@roxi/routify/typings/runtime/store'
+  export const Router: import('svelte/internal').SvelteComponent
+  export const routify: typeof import('@roxi/routify/plugins/rollup')
+
+  global {
+    export const $page: ClientNodeApi
+    export const $layout: ClientNodeApi
+    export const $params: import('./typings/runtime/helpers').ParamsHelper
+    export const $leftover: import('./typings/runtime/helpers').LeftoverHelper
+    export const $url: import('./typings/runtime/helpers').UrlHelper
+    export const $isActive: import('./typings/runtime/helpers').IsActiveHelper
+    export const $goto: import('./typings/runtime/helpers').GotoHelper
+    export const $beforeUrlChange: import('./typings/runtime/helpers').BeforeUrlChangeHelper
+    export const $ready: import('./typings/runtime/helpers').ReadyHelper
+  }
 }
 
 declare module '@roxi/routify/tmp/routes' {
-    export const tree: ClientNode
-    export const routes: ClientNode[]
+  export const tree: ClientNode
+  export const routes: ClientNode[]
+}
+
+declare module '@roxi/routify/hmr' {
+  let hmr: typeof import('./typings/hmr')
+  export default hmr
 }
