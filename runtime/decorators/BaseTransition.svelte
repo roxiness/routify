94269c386659d24fa2cbface371204a4d267bffd
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-02-11 11:02:18 +0100

    fix: hide scrollbars on baseTransition

--- runtime/decorators/BaseTransition.svelte
+++ runtime/decorators/BaseTransition.svelte
@@ -35,19 +35,30 @@
   $: normalizedConfig = { ...defaultConfig, ..._config }
   $: ({ transition, inParams, outParams } = normalizedConfig)
 
-  function setAbsolute({ target }) {
+  function setFixed({ target }) {
     const rect = target.getBoundingClientRect()
+    target.style.position = 'fixed'
     target.style.width = `${rect.width}px`
     target.style.height = `${rect.height}px`
     target.style.top = `${rect.top}px`
     target.style.left = `${rect.left}px`
-    target.style.position = 'fixed'
   }
-  function removeAbsolute({ target }) {
-    target.style.position = ''
-    target.style.width = ''
-    target.style.height = ''
-    target.style.transform = ''
+  function undoFixed({ target }) {
+    target.style.position = null
+    target.style.width = null
+    target.style.height = null
+    target.style.top = null
+    target.style.left = null
+  }
+
+  let parentElementOverflow = null
+  function setHidden({ target }) {
+    if (parentElementOverflow === null)
+      parentElementOverflow = target.parentElement.style.overflow
+    target.parentElement.style.overflow = 'hidden'
+  }
+  function undoHidden({ target }) {
+    target.parentElement.style.overflow = parentElementOverflow
   }
 </script>
 
@@ -55,8 +66,12 @@
   class="transition node{get(node).__file.id}"
   in:transition|local={inParams}
   out:transition|local={outParams}
-  on:introstart={removeAbsolute}
-  on:outrostart={setAbsolute}
+  on:introstart={(t) => {
+    undoFixed(t)
+    setHidden(t)
+  }}
+  on:introend={undoHidden}
+  on:outrostart={setFixed}
 >
   <slot />
 </div>
