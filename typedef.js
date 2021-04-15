49574898a1df9c9afba50b37b7bc23ea0ad48598
Jakob Rosenberg <jakob@pycom.io>
2021-04-15 20:27:37 +0200

    typedef

--- typedef.js
+++ typedef.js
@@ -1,7 +1,19 @@
 /**
+ * @typedef {import('./common/Routify')['Routify']['prototype']} Routify
+ */
+
+/**
+ * @template T
+ * @callback RoutifyCallback
+ * @param {{instance: Routify}} first
+ * @returns {T|Promise<T>}
+ */
+
+/**
  * @typedef {Object} RoutifyPlugin
- * @prop {string|string[]} before
- * @prop {string|string[]} after
- * @prop {Promise<function():Boolean>} condition
- * @prop {Promise<function():any>} run
+ * @prop {string|string[]=} before
+ * @prop {string|string[]=} after
+ * @prop {RoutifyCallback<Boolean>=} condition
+ * @prop {RoutifyCallback<any>} run
+ * @prop {'compile'|'runtime'} mode
  */
