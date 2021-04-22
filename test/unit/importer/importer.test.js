8552ecc332ce9be0fbb71e73aea1e724e3bebc88
Jakob Rosenberg <jakob@pycom.io>
2021-04-22 16:13:49 +0200

    test: added more importer tests

--- test/unit/importer/importer.test.js
+++ test/unit/importer/importer.test.js
@@ -1,26 +1,26 @@
 import { suite } from 'uvu'
 import * as assert from 'uvu/assert'
-import { importer, populateNodeTreeFromSnapshot } from '../../../plugins/importer/importer.js'
+import { populateNodeTreeFromSnapshot } from '../../../plugins/importer/importer.js'
 import { RoutifyRuntime } from '../../../runtime/RoutifyRuntime.js'
 
 const test = suite('importer')
 
 const exported = {
-    'meta': {},
-    'component': '_default',
-    'id': '_default',
-    'rootName': 'default',
-    'file': { 'path': 'test/unit/exporter/example/_module.svelte' },
-    'children': [
+    meta: {},
+    component: '_default',
+    id: '_default',
+    rootName: 'default',
+    file: { path: 'test/unit/exporter/example/_module.svelte' },
+    children: [
         {
-            'meta': { 'reset': true },
-            'component': '_default_admin',
-            'id': '_default_admin',
-            'name': 'admin',
-            'file': { 'path': 'test/unit/exporter/example/admin/_reset.svelte', },
-            'children': []
-        }
-    ]
+            meta: { reset: true },
+            component: '_default_admin',
+            id: '_default_admin',
+            name: 'admin',
+            file: { path: 'test/unit/exporter/example/admin/_reset.svelte' },
+            children: [],
+        },
+    ],
 }
 
 test('populateNodeTreeFromSnapshot', () => {
@@ -34,10 +34,20 @@ test('populateNodeTreeFromSnapshot', () => {
 
 test('can import', () => {
     const instance = new RoutifyRuntime({ routes: exported })
-    importer({ instance })
-
+    assert.is(instance.superNode.children.length, 1)
     assert.is(instance.superNode.children[0].id, '_default')
     assert.is(instance.superNode.children[0].children[0].id, '_default_admin')
+})
+
+test('nodes created by instance have correct constructor', () => {
+    const instance = new RoutifyRuntime({ routes: exported })
+    const child = instance.superNode.createChild('test')
+    assert.is(child.constructor.name, 'RNodeRuntime')
+})
+
+test('imported nodes have correct constructor', () => {
+    const instance = new RoutifyRuntime({ routes: exported })
+    assert.is(instance.superNode.children[0].constructor.name, 'RNodeRuntime')
 })
 
 test.run()
