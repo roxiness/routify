5c1b044fc19fbe726d09d327eb3c97b32a7307f8
Jakob Rosenberg <jakob@pycom.io>
2021-04-23 09:24:51 +0200

    cleanup

--- example/src/routes/_module.svelte
+++ example/src/routes/_module.svelte
@@ -3,16 +3,7 @@
     export let payload
 </script>
 
-<FileTree node={payload.node} />
-
 <h1>Module</h1>
-
-<nav>
-    <a href="/">home</a>
-    <a href="/file1">file1</a>
-    <a href="/folder1/">folder1</a>
-    <a href="/folder1/file1">folder1/file1</a>
-    <a href="/folder1/my-title">folder1/my-title</a>
-</nav>
+<FileTree node={payload.node} />
 
 <slot><!-- optional fallback --></slot>
