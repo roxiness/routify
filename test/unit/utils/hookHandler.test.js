7467a1a266c4314c7f0047eff33d4b47ba6b3606
Jakob Rosenberg <jakob@pycom.io>
2021-04-23 14:20:55 +0200

    feat: added hookHandler

---
+++ test/unit/utils/hookHandler.test.js
@@ -0,0 +1,33 @@
+import { suite } from 'uvu'
+import * as assert from 'uvu/assert'
+import { hookHandler } from '../../../lib/utils.js'
+import '../../../typedef.js'
+
+const test = suite('hookHandler')
+
+test('can create a hook', () => {
+    let counter = 0
+    const hook = hookHandler()
+    const unsub = hook(() => counter++)
+    assert.is(counter, 0)
+    hook.callCallbacks()
+    hook.callCallbacks()
+    hook.callCallbacks()
+    assert.is(counter, 3)
+    unsub()
+    hook.callCallbacks()
+    hook.callCallbacks()
+    assert.is(counter, 3)
+})
+
+test('can pass params in a hook', () => {
+    let received = ''
+    const hook = hookHandler()
+    const unsub = hook(val => (received = val))
+    assert.is(received, '')
+    hook.callCallbacks('new value')
+    assert.is(received, 'new value')
+    unsub()
+})
+
+test.run()
