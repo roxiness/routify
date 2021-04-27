8b72e97e9b01e7cef70a65775dcbdbf32b7b597b
Ales Vaupotic <ales@vaupotic.eu>
2021-04-28 01:58:29 +0200

    added vite restart

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import svelte from '@sveltejs/vite-plugin-svelte'
+import pkg from 'vite-plugin-restart'
+const VitePluginRestart = pkg.default
 
 const production = process.env.NODE_ENV === 'production'
 
@@ -9,6 +11,7 @@ export default defineConfig({
     },
     clearScreen: false,
     plugins: [
+        VitePluginRestart({ restart: ['../lib/**/*'] }),
         svelte({
             emitCss: true,
             compilerOptions: {
