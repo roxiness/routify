134236fa39f223a2cec8d97c847ba10d14f0d7a5
Jakob Rosenberg <jakob@pycom.io>
2021-04-27 10:49:11 +0200

    test: fix bad name

--- test/integration/meta/example/compiled.meta.js
+++ test/integration/meta/example/compiled.meta.js
@@ -1,7 +1,7 @@
 export default async function () {
     await new Promise(resolve => setTimeout(resolve, 1))
     return {
-        asyncData: 'Im async',
+        plain: 'Im plain',
         'asyncDataSplit|split': 'Im async split' + 1 + 2 + 3,
     }
 }
