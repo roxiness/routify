75685aa18f0766e16c2e531426e2e21c24b04749
Ales Vaupotic <ales@vaupotic.eu>
2021-04-27 23:29:14 +0200

    moved index to root, replaced rollup with vite

---
+++ example/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import svelte from '@sveltejs/vite-plugin-svelte'
+
+const production = process.env.NODE_ENV === 'production'
+
+export default defineConfig({
+    server: {
+        open: true,
+    },
+    clearScreen: false,
+    plugins: [
+        svelte({
+            emitCss: true,
+            compilerOptions: {
+                dev: !production,
+            },
+        }),
+    ],
+    build: {
+        polyfillDynamicImport: false,
+        cssCodeSplit: false,
+    },
+    resolve: {
+        dedupe: ['svelte'],
+    },
+})
