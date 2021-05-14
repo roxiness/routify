80775b7d01d91e6a1b3fce6ee2913c567b03c1da
Jakob Rosenberg <jakob@pycom.io>
2021-05-14 22:34:28 +0200

    feat: added multi router example

--- example/src/routes/experiments/multi-router/index.svelte
+++ example/src/routes/experiments/multi-router/index.svelte
@@ -1,8 +1,51 @@
 <script>
     import { Router } from '@roxi/routify'
+    let leftRouter = {}
+    let rightRouter = {}
+    let inputLeft = ''
+    let inputRight = ''
+    $: ({ leftActiveUrl } = leftRouter)
+    $: ({ rightActiveUrl } = rightRouter)
+
+    $: setInputLeft(leftActiveUrl && $leftActiveUrl.url)
+    $: setInputRight(rightActiveUrl && $rightActiveUrl.url)
+    const setInputLeft = val => (inputLeft = val)
+    const setInputRight = val => (inputRight = val)
+
+    const submitLeft = () =>
+        leftActiveUrl.set(inputLeft, 'pushState', 'internal')
+
+    const submitRight = () =>
+        rightActiveUrl.set(inputRight, 'pushState', 'internal')
 </script>
 
 <h1>Multi router</h1>
 
-<Router name="nested1" url="/" />
-<Router name="nested2" url="/" />
+<div class="split">
+    <div class="left">
+        <form on:submit|preventDefault={submitLeft}>
+            <input type="text" bind:value={inputLeft} />
+        </form>
+        <Router name="nested1" bind:router={leftRouter} />
+    </div>
+    <div class="right">
+        <form on:submit|preventDefault={submitRight}>
+            <input type="text" bind:value={inputRight} />
+        </form>
+        <Router name="nested2" bind:router={rightRouter} />
+    </div>
+</div>
+
+<style>
+    .split {
+        display: flex;
+    }
+    .split > * {
+        flex-grow: 1;
+        flex-basis: 0;
+        margin: 0 16px;
+    }
+    input {
+        background: white;
+    }
+</style>
