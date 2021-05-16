1c2e51dff4b5fa4b9c16cb5876a7e16efba07cd2
Jakob Rosenberg <jakob@pycom.io>
2021-05-16 09:35:54 +0200

    feat: reflectors

--- example/src/routes/experiments/multi-router/_options.svelte
+++ example/src/routes/experiments/multi-router/_options.svelte
@@ -1,21 +1,45 @@
 <script>
+    import { derived } from 'svelte/store'
+    import {
+        AddressReflector,
+        InternalReflector,
+        LocalStorageReflector,
+    } from '@roxi/routify'
+
+    const reflectors = {
+        AddressReflector,
+        InternalReflector,
+        LocalStorageReflector,
+    }
+
     export let router
-    const urlHook = [
-        { name: 'address' },
-        { name: 'internal' },
-        { name: 'localStorage' },
-    ]
-    console.log({ router })
+
+    const urlHooks = derived(router.urlReflector, urlReflector => {
+        return Object.entries(reflectors).map(([name, reflector]) => ({
+            name,
+            isActive: urlReflector.constructor.name === name,
+            reflector,
+        }))
+    })
+
+    const handleClick = reflector => {
+        console.log('hello')
+        router.urlReflector = reflector
+    }
 </script>
 
 <div class="options">
     <div class="cog">⚙</div>
     <ul class="menu">
-        <li>address</li>
-        <li>internal</li>
-        <li>localStorage</li>
+        {#each $urlHooks as { name, isActive, reflector }}
+            <li class:isActive on:click={() => handleClick(reflector)}>
+                {name}
+            </li>
+        {/each}
     </ul>
 </div>
+
+{JSON.stringify(router.urlReflector)}
 
 <style>
     .options {
@@ -31,5 +55,8 @@
         padding: 8px 20px;
         list-style: none;
         border-radius: 4px;
+    }
+    .isActive {
+        font-weight: bold;
     }
 </style>
