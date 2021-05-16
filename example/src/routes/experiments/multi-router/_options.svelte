1de8427f0c068fe2ee5fa3031ba7dc81444b6809
Jakob Rosenberg <jakob@pycom.io>
2021-05-16 16:54:22 +0200

    refactor: polish mini browser drop down

--- example/src/routes/experiments/multi-router/_options.svelte
+++ example/src/routes/experiments/multi-router/_options.svelte
@@ -1,5 +1,6 @@
 <script>
     import { derived } from 'svelte/store'
+    import { slide } from 'svelte/transition'
     import {
         AddressReflector,
         InternalReflector,
@@ -33,7 +34,8 @@
 <div class="options">
     <div class="cog" on:click={toggleMenu}>⚙</div>
     {#if show}
-        <ul class="menu">
+        <div class="backdrop" on:click={toggleMenu} />
+        <ul class="menu" transition:slide|local={{ duration: 65 }}>
             {#each $urlHooks as { name, isActive, reflector }}
                 <li class:isActive on:click={() => selectReflector(reflector)}>
                     {name}
@@ -44,21 +46,50 @@
 </div>
 
 <style>
-    .cog,
-    li {
+    .backdrop {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+    }
+    .cog {
+        padding: 4px 8px 14px;
         cursor: pointer;
+    }
+    li {
+        position: relative;
+        cursor: pointer;
+        margin: 0;
+        /* min-height: 32px; */
+        line-height: 40px;
+        padding: 0 20px;
+        border-left: 3px solid rgba(0, 0, 0, 0);
+    }
+    li:first-of-type {
+        border-radius: 4px 0 0 0;
+        padding-top: 8px;
+    }
+    li:last-of-type {
+        border-radius: 0 0 0 4px;
+        padding-bottom: 8px;
+    }
+    li:hover {
+        border-left: 3px solid rgb(230, 230, 230);
+        background: rgba(0, 0, 0, 0.018);
+        /* list-style-type: '⬛'; */
     }
     .options {
         position: absolute;
-        top: 4px;
-        right: 8px;
+        top: 0px;
+        right: 0px;
     }
     .menu {
         position: absolute;
         right: 0;
         background: white;
         box-shadow: 0 0 5px 5px rgba(0, 0, 0, 0.02);
-        padding: 8px 20px;
+
         list-style: none;
         border-radius: 4px;
     }
