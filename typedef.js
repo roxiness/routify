9994d80b34df47311ff62c7c132adc72d51a4f6b
Jakob Rosenberg <jakob@pycom.io>
2021-06-13 23:16:02 +0200

    chore: define plugins

--- typedef.js
+++ typedef.js
@@ -36,13 +36,100 @@
  * @returns {T|Promise<T>}
  */
 
+/*********************
+ * BUILDTIME OPTIONS *
+ *********************/
+
 /**
- * @typedef {Object} RoutifyPlugin
+ * @typedef {Object} RoutifyBuildtimeOptions
+ * @prop {string} routifyDir defaults to '.routify'
+ * @prop {object} filemapper
+ * @prop {string[]} filemapper.moduleFiles defaults to ['_module.svelte', '_reset.svelte']
+ * @prop {string[]} filemapper.resetFiles defaults to ['_reset.svelte']
+ * @prop {string|Object<string,string>} routesDir defaults to { default: 'src/routes' }
+ * @prop {string[]} extensions defaults to ['.svelte', '.html', '.md', '.svx'],
+ * @prop {string[]|Object[]} plugins
+ * @prop {boolean} watch rebuild Routify routes on changes
+ */
+
+/*******************
+ * RUNTIME OPTIONS *
+ *******************/
+/**
+ * @typedef {Object} RoutifyRuntimeOptions
+ * @prop {function(RoutifyRuntime):void} init
+ * @prop {UrlTransform|UrlTransform[]} urlTransform
+ * @prop {QueryHandler|QueryHandler[]} queryHandler
+ * @prop {function} beforeRouteChange
+ * @prop {function} afterRouteChange
+ */
+
+/********************************
+ * ROUTIFY EXTERNAL META HELPER *
+ ********************************/
+/**
+ * @typedef {Object} RoutifyExternalMetaHelper
+ * @prop {RoutifyRuntime} instance
+ * @prop {options} options
+ * @prop {string} tempPath
+ */
+
+/**********
+ * PLUGIN *
+ **********/
+
+/**
+ * @typedef {RoutifyBasePlugin & RoutifyRuntimeOptions} RoutifyRuntimePlugin
+ */
+
+/**
+ * @typedef {Object} RoutifyBasePlugin
  * @prop {string|string[]=} before name of plugin(s) to run before
  * @prop {string|string[]=} after name of plugin(s) to run after
- * @prop {RoutifyCallback<Boolean>=} condition run plugin if true
- * @prop {RoutifyCallback<any>} run plugin script
- * @prop {'compile'|'runtime'} mode
+ */
+
+/**
+ * @typedef {Object} RoutifyBuildtimePluginType
+ * @prop {function(RoutifyBuildtime)} build
+ * @prop {RoutifyExternalMetaHelper} meta
+ */
+
+/*****************
+ * URL TRANSFORM *
+ *****************/
+
+/**
+ * @callback UrlTransformFn
+ * @param {string} url
+ * @returns {string}
+ */
+
+/**
+ * @typedef {Object} UrlTransform
+ * @prop {UrlTransformFn} toInternal
+ * @prop {UrlTransformFn} toExgternal
+ */
+
+/*****************
+ * QUERY HANDLER *
+ *****************/
+
+/**
+ * @typedef {Object} QueryHandler
+ * @prop {QueryHandlerParse} parse
+ * @prop {QueryHandlerStringify} stringify
+ */
+
+/**
+ * @callback QueryHandlerParse
+ * @param {string} search
+ * @return {Object<string, string>}
+ */
+
+/**
+ * @callback QueryHandlerStringify
+ * @param {Object<string, string>} search
+ * @return {string}
  */
 
 /**
