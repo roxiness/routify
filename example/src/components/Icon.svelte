8aefbb3d22d7fbd05e4ce55c7c91f63bb1a813d1
Jakob Rosenberg <jakob@pycom.io>
2021-07-06 23:42:24 +0200

    example: add icons component

---
+++ example/src/components/Icon.svelte
@@ -0,0 +1,18 @@
+<script>
+    import * as icons from './icons/index.js'
+    export let name
+</script>
+
+<span>
+    {@html icons[name]}
+</span>
+
+<style>
+    span {
+        line-height: 0;
+    }
+    span :global(svg) {
+        /* stroke-width: 2px; */
+        /* stroke: #636c75; */
+    }
+</style>
