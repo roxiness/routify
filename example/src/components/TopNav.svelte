6bb213b3937fc79c7910fa764861f544e95afadd
Jakob Rosenberg <jakob@pycom.io>
2021-07-06 23:44:05 +0200

    example: styling

---
+++ example/src/components/TopNav.svelte
@@ -0,0 +1,52 @@
+<script>
+    import Icon from '#cmp/Icon.svelte'
+</script>
+
+<nav>
+    <div class="container">
+        <img src="/assets/routify3.svg" height="56px" />
+        <div class="middle">
+            <a href="https://github.com/roxiness/routify">
+                <Icon name="IconGithub" />
+            </a>
+            <a href="https://discord.gg/ntKJD5B">
+                <Icon name="IconDiscord" />
+            </a>
+            <a href="https://twitter.com/routifyjs">
+                <Icon name="IconTwitter" />
+            </a>
+        </div>
+    </div>
+</nav>
+
+<style>
+    nav {
+        height: var(--spacing-8);
+        box-shadow: 0 0 16px rgba(0, 0, 0, 0.1);
+        border-bottom: solid 1px #ddd;
+        align-items: center;
+        display: flex;
+    }
+    .container {
+        grid-template-columns: var(--left-nav) auto;
+        display: grid;
+    }
+    .middle {
+        display: flex;
+        padding-left: calc(var(--spacing-8) - 12px);
+        align-items: center;
+    }
+    .middle a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .middle a {
+        opacity: 0.5;
+        width: 48px;
+        height: 48px;
+    }
+    .middle a:hover {
+        opacity: 1;
+    }
+</style>
--- example/src/routes/_module.svelte
+++ example/src/routes/_module.svelte
@@ -1,54 +1,44 @@
 <script>
-    import FileTree from '../components/FileTree.svelte'
+    import FileTree from '#cmp/FileTree.svelte'
+    import TopNav from '#cmp/TopNav.svelte'
     import { SideNav } from 'polykit'
     export let context
 </script>
 
-<!-- <SideNav class="shadow bg-white">
-    <aside slot="aside">
-        <h1>Routify 3</h1>
-        <FileTree node={context.node} />
-    </aside>
-    <main class="bg-gray-100  p-8">
-        <slot />
-    </main>
-</SideNav> -->
+<!-- routify:meta split|scoped -->
 
 <div class="app">
-    <aside>
-        <img src="/assets/routify3.svg" />
-        <br />
-        <br />
-        <FileTree node={context.node} />
-    </aside>
-    <main>
-        <slot />
-    </main>
+    <TopNav />
+    <div class="container main">
+        <aside>
+            <br />
+            <br />
+            <FileTree node={context.node} />
+        </aside>
+        <main>
+            <slot />
+        </main>
+    </div>
 </div>
 
-<!-- routify:meta split|scoped -->
 <style>
-    h1 {
-        white-space: nowrap;
+    :global(.container) {
+        max-width: 1240px;
     }
     .app {
-        display: grid;
-        grid-template-columns: min-content auto;
         height: 100vh;
-        max-width: 1280px;
-        color: var(--indigo-300);
+    }
+    .main {
+        display: grid;
+        grid-template-columns: var(--left-nav) auto;
+        color: var(--color-grey-700);
         margin-left: auto;
         margin-right: auto;
-    }
-    aside {
-        width: var(--spacing-11);
-        padding-top: var(--spacing-7);
-        padding-left: var(--spacing-5);
     }
     main {
         background: #f4f7f9;
         border-radius: var(--spacing-5);
-        margin: var(--spacing-5);
+        margin: var(--spacing-5) 0;
         padding: var(--spacing-6) var(--spacing-8);
         box-sizing: border-box;
     }
