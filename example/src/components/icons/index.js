84dcb1e05f8fea1961cc57ca3c060f40eb2a620c
Jakob Rosenberg <jakob@pycom.io>
2021-07-06 23:42:09 +0200

    example: move icons

---
+++ example/src/components/icons/index.js
@@ -0,0 +1,18 @@
+export { default as IconChevronUp } from "./chevron-up.svg";
+export { default as IconChevronDown } from "./chevron-down.svg";
+export { default as IconChevronLeft } from "./chevron-left.svg";
+export { default as IconChevronRight } from "./chevron-right.svg";
+export { default as IconInfo } from "./info.svg";
+export { default as IconWarning } from "./warning.svg";
+
+export { default as IconDiscord } from "./discord.svg";
+export { default as IconTwitter } from "./twitter.svg";
+export { default as IconGithub } from "./github.svg";
+
+
+export { default as IconResolve } from "./resolve.svg";
+export { default as IconFolder } from "./folder.svg";
+export { default as IconFile } from "./file.svg";
+export { default as IconUrl } from "./url.svg";
+
+export { default as IconSearch } from "./search.svg";
