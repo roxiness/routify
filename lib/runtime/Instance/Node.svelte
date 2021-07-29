9ee755c225f3a3257852478b10f5a790ac925e1b
Jakob Rosenberg <jakob@pycom.io>
2021-07-29 12:56:32 +0200

    fix: set context from Node.svelte

--- lib/runtime/Instance/Node.svelte
+++ lib/runtime/Instance/Node.svelte
@@ -1,10 +1,14 @@
 <script>
+    import { getContext, setContext } from 'svelte'
     export let node
     export let passthrough
+    const context = getContext('routify-fragment-context') || {}
+    context.node = node
+    setContext('routify-fragment-context', context)
 </script>
 
 {#await node.rawComponent then rawComponent}
-    <svelte:component this={rawComponent} {...passthrough}>
+    <svelte:component this={rawComponent} {...passthrough} {context}>
         <slot />
     </svelte:component>
 {/await}
