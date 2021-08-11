3aa7ed838d51652ed46448a7a1fb0575a2df3661
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-08-11 23:16:13 +0200

    fix: programmatic components flakiness

--- lib/runtime/Instance/Node.svelte
+++ lib/runtime/Instance/Node.svelte
@@ -2,13 +2,18 @@
     import { getContext, setContext } from 'svelte'
     export let node
     export let passthrough
+
     const context = { ...getContext('routify-fragment-context') }
     context.node = node
     setContext('routify-fragment-context', context)
 </script>
 
-{#await node.rawComponent then rawComponent}
-    <svelte:component this={rawComponent} {...passthrough} {context}>
-        <slot />
-    </svelte:component>
-{/await}
+{#if node.module}
+    {#await node.rawComponent then rawComponent}
+        <svelte:component this={rawComponent} {...passthrough} {context}>
+            <slot />
+        </svelte:component>
+    {/await}
+{:else}
+    <slot />
+{/if}
