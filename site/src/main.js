8538d0a72ac7145e8519029f04cc85a3054ad2a1
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-08-28 23:04:53 +0200

    doc: temp warning fix

--- site/src/main.js
+++ site/src/main.js
@@ -5,3 +5,16 @@ import './components/prismjs.css'
 const app = new App({ target: document.body })
 
 export default app
+
+const _warn = console.warn
+
+console.warn = (msg, ...params) => {
+    const msgs = ["unknown prop 'context'", 'unexpected slot "default"'].join('|')
+
+    if (!msg.match(msgs)) {
+        const log = new Error()
+        _warn.bind(console)(
+            'Error (not really an error) ' + msg + '\n' + log.stack.split('\n')[2],
+        )
+    }
+}
