10f2777aaaacc5a015837bcf3aedabbb758503b1
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-09-10 14:55:38 +0200

    chore: initial

---
+++ examples/microframework/app/vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { mdsvex } from 'mdsvex'
+import routify from '@roxi/routify/vite-plugin'
+
+const production = process.env.NODE_ENV === 'production'
+
+export default defineConfig({
+    clearScreen: false,
+    plugins: [
+        routify({
+            routesDir: {
+                default: './src/routes',
+                moduleA: '../module/src/routes',
+            },
+        }),
+        svelte({
+            emitCss: true,
+            compilerOptions: {
+                dev: !production,
+            },
+            extensions: ['.md', '.svelte'],
+            preprocess: [mdsvex({ extension: 'md' })],
+        }),
+    ],
+    server: { port: 1337 },
+    resolve: {
+        alias: {
+            '#cmp': process.cwd() + '/src/components',
+        },
+    },
+})
