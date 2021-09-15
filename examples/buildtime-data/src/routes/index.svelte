5c59dfe5bd975e6ee3885fdafb88f21358568911
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-09-15 08:49:34 +0200

    chore: update buildtime-data example

--- examples/buildtime-data/src/routes/index.svelte
+++ examples/buildtime-data/src/routes/index.svelte
@@ -1,10 +1,26 @@
 <script>
     import { meta } from '@roxi/routify'
+    let show
     console.log($meta)
 </script>
 
 <h1>Routify 3 App</h1>
 
+<h4>Plain meta</h4>
 <pre>
-    {JSON.stringify($meta, null, 2)}
+    {JSON.stringify($meta, null, 2)}    
 </pre>
+
+<br />
+<br />
+
+{#if !show}
+    <button on:click={() => (show = true)}>show dynamic imported meta</button>
+{:else}
+    <h4>dynamic imported meta</h4>
+    <pre>
+    {#await $meta.luke.data() then data}
+    {JSON.stringify(data, null, 2)}
+    {/await}
+</pre>
+{/if}
