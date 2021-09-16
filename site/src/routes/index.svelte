cf13117551b808254d2dbcf34ab5cee05c747e28
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-09-16 21:07:40 +0200

    chore: update frontpage slightly

--- site/src/routes/index.svelte
+++ site/src/routes/index.svelte
@@ -1,5 +1,16 @@
+<script>
+    import A from './docs/[...index].svelte'
+</script>
+
 <div class="container">
     <img class="brand" src="/assets/routify3.svg" width="600" />
+    <h1>Work in Progress</h1>
+    <a href="/docs">DOCS</a>
 </div>
 
-# Auto generated node tree ### Resolve ### Traverse
+<style>
+    .container {
+        margin-top: 10rem;
+        text-align: center;
+    }
+</style>
