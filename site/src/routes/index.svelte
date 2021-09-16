497194999329409f5d4eae15b2e3ed115b97a2eb
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-09-16 19:35:38 +0200

    refactor: rename index.md to index.svelte

--- site/src/routes/index.svelte
+++ site/src/routes/index.svelte
@@ -2,7 +2,4 @@
     <img class="brand" src="/assets/routify3.svg" width="600" />
 </div>
 
-# Auto generated node tree
-### Resolve
-### Traverse
-
+# Auto generated node tree ### Resolve ### Traverse
