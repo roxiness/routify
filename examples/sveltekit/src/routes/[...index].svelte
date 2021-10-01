9641eb556603564395118dbe7e9711cf94e88c7a
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-10-01 20:26:50 +0200

    added SSR to Sveltekit example

--- examples/sveltekit/src/routes/[...index].svelte
+++ examples/sveltekit/src/routes/[...index].svelte
@@ -1,6 +1,11 @@
-<script>
-    import { Router } from '@roxi/routify'
+<script context="module">
+    import { Router, RouterClass } from '@roxi/routify'
     import routes from '../../.routify/routes.default.js'
+
+    const router = new RouterClass({ routes })
+
+    // for SSR we need to tell Sveltekit to wait for Routify to finish loading its components
+    export const load = payload => router.url.replace(payload.page.path)
 </script>
 
-<Router {routes} />
+<Router {router} />
