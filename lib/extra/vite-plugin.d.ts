8383a33fb6483de69622d869c3183e9a44db9d33
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-10-08 22:41:02 +0200

    fix: types for vite plugin

---
+++ lib/extra/vite-plugin.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="@roxi/routify/typings/typedef.d.ts" />
+/// <reference path="../../typings/typedef.d.ts" />
+
+import * as module from '../../typings/lib/extra/vite-plugin'
+export default module.default
