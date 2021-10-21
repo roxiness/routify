09fee5aa8af1e6d78b10d1d00ff5b21283e2e7ae
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-10-21 13:07:06 -0700

    Disable SvelteKit's router in example

--- examples/sveltekit/svelte.config.js
+++ examples/sveltekit/svelte.config.js
@@ -5,14 +5,16 @@ const config = {
     kit: {
         // hydrate the <div id="svelte"> element in src/app.html
         target: '#svelte',
-
+        
         vite: {
             plugins: [routify()],
             resolve: {
                 dedupe: ['svelte'],
             },
         },
-    },
-}
+
+        router: false
+    }
+};
 
 export default config
