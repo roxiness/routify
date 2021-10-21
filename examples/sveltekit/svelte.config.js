75af21ecfb0928a2e83c445ce2f9584d3d1bd121
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-10-21 13:08:07 -0700

    remove whitespace

--- examples/sveltekit/svelte.config.js
+++ examples/sveltekit/svelte.config.js
@@ -5,7 +5,7 @@ const config = {
     kit: {
         // hydrate the <div id="svelte"> element in src/app.html
         target: '#svelte',
-        
+
         vite: {
             plugins: [routify()],
             resolve: {
