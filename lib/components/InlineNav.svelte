c33830cbf5f9065ac52fd36b2c48b8f0f04f8494
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-11-04 21:13:21 +0100

    fix: improve inlineNav

--- lib/components/InlineNav.svelte
+++ lib/components/InlineNav.svelte
@@ -3,23 +3,32 @@
     import { InternalReflector } from '@roxi/routify/lib/runtime/Router/urlReflectors/Internal'
     import Nested from './Nested.svelte'
     export let parentNode = null
+    export let singlePage = null
 
     const { route, node } = $context
     const { activeRoute } = route.router
 
+    singlePage = singlePage || typeof window === 'undefined'
     parentNode = parentNode || node
 
     /**
+     * we're going to add the router class to the Router
+     * component before we send it back as "page"
      * @returns {Router['constructor']}
      * */
-    const createRouterCmp = options => {
+    const createRouterCmp = router => {
         return function (opts) {
-            opts.props = opts.props || {}
-            opts.props.router = options.router
+            opts.props = { ...opts.props, router }
+
             return new Router(opts)
         }
     }
 
+    /**
+     * before render, we need to strip the components
+     * that have already been rendered by the parent router
+     * @param fragments
+     */
     const beforeRender = fragments => {
         const cutoff = fragments.findIndex(
             fragment => fragment.node.level > parentNode.level,
@@ -34,21 +43,31 @@
             passthrough: true,
             urlReflector: InternalReflector,
             url: n.path,
+            name: n.name,
         }),
     }))
 
-    $: pages =
-        typeof window !== 'undefined'
-            ? routers.map(page => ({ Page: createRouterCmp({ router: page.router }) }))
-            : [{ Page: Nested }]
-
-    $: activeSubRoute = routers.find(p =>
+    $: activeSubRouter = routers.find(p =>
         $activeRoute.allFragments.map(ar => ar.node).includes(p.rootNode),
     )
 
-    $: index = routers.findIndex(r => r === activeSubRoute)
-    $: console.log(activeSubRoute)
-    $: console.log({ index })
+    $: activeSubRouter.router.activeRoute.set($activeRoute)
+
+    $: payload = {
+        index: singlePage ? 0 : routers.findIndex(r => r === activeSubRouter),
+        pages: !singlePage
+            ? routers.map(page => ({ Page: createRouterCmp(page.router) }))
+            : [{ Page: Nested }],
+        inBrowser: !singlePage,
+    }
+
+    let id = singlePage
 </script>
 
-<slot {pages} {index} />
+{#if !singlePage}
+    <div style="display: contents">
+        <slot {...payload} />
+    </div>
+{:else}
+    <slot {...payload} />
+{/if}
