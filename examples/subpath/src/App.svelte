e284d1bea3d4db16e6ebb54f3f21d2142ebd7bbe
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-11-12 17:49:17 +0100

    chore: add-subpath-example

---
+++ examples/subpath/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { Router } from '@roxi/routify'
+    import routes from '../.routify/routes.default.js'
+
+    /** @type {UrlRewrite}*/
+    const urlRewrite = {
+        toExternal: url => '/subpath' + url,
+        toInternal: url => url.replace(/^\/subpath/, ''),
+    }
+</script>
+
+<Router {routes} {urlRewrite} />
---
+++ examples/subpath/src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { url } from '@roxi/routify'
+</script>
+
+<h1>Subpath Example</h1>
+
+<a href={$url('../foo')}>{$url('../foo')}</a>
