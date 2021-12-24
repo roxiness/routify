3db33ea0d6d0ea3dfe6087bc5c273c6f6cad2b0e
jakobrosenberg <jakobrosenberg@gmail.com>
2021-12-24 21:57:13 +0000

    chore: kitchensink dynamic paths

---
+++ examples/kitchensink/src/routes/paths/index.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { url } from '@roxi/routify'
+    const urls = {
+        plain: $url('../plain'),
+        dynamic: $url('../[country]-[language]/[post]', {
+            country: 'UK',
+            language: 'en',
+            post: 'Hello World',
+        }),
+        overload: $url('../[country]-[language]/[post]', {
+            country: 'UK',
+            language: 'en',
+            post: 'Hello World',
+            theme: 'dark',
+            author: 'john doe',
+        }),
+    }
+</script>
+
+<a href={urls.plain}>{urls.plain}</a>
+<a href={urls.dynamic}>{urls.dynamic}</a>
+<a href={urls.overload}>{urls.overload}</a>
+
+<style>
+    a {
+        display: block;
+    }
+</style>
