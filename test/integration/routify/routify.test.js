9fd73578be22959dacfa526db3a5c38fe314616c
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-12-31 18:02:29 +0100

    feat: added transform option to buildtime plugins

--- test/integration/routify/routify.test.js
+++ test/integration/routify/routify.test.js
@@ -25,3 +25,57 @@ test('can run routify with bundled plugins', async () => {
         ),
     ).toMatchSnapshot('bundles')
 })
+
+test('can transform output files', async () => {
+    const bundlePath = resolve(
+        __dirname,
+        'temp',
+        '.routify-transform',
+        'bundles',
+        '_default_admin-bundle.js',
+    )
+
+    const baseInstance = new RoutifyBuildtime({
+        routifyDir: resolve(__dirname, 'temp', '.routify-transform'),
+        routesDir: resolve(__dirname, 'example'),
+    })
+    await baseInstance.start()
+
+    let baseExpectedContent = readFileSync(bundlePath, 'utf-8')
+    expect(baseExpectedContent).toMatchSnapshot('routify-transform')
+
+    let ids = []
+
+    const emptyTransformInstance = new RoutifyBuildtime({
+        routifyDir: resolve(__dirname, 'temp', '.routify-transform'),
+        routesDir: resolve(__dirname, 'example'),
+        plugins: [
+            {
+                transform: (id, content) => {
+                    ids.push(id)
+                },
+            },
+        ],
+    })
+
+    expect(ids[0]).toMatch(/_default_admin-bundle.js$/)
+    expect(ids[1]).toMatch(/routes.default.js$/)
+    expect(ids[2]).toMatch(/instance.default.js$/)
+
+    await emptyTransformInstance.start()
+    const emptyTransformExpectedContent = readFileSync(bundlePath, 'utf-8')
+    expect(emptyTransformExpectedContent).toEqual(baseExpectedContent)
+
+    const transformInstance = new RoutifyBuildtime({
+        routifyDir: resolve(__dirname, 'temp', '.routify-transform'),
+        routesDir: resolve(__dirname, 'example'),
+        plugins: [
+            {
+                transform: (id, content) => content + '\nhello',
+            },
+        ],
+    })
+    await transformInstance.start()
+    const transformExpectedContent = readFileSync(bundlePath, 'utf-8')
+    expect(transformExpectedContent).toEqual(baseExpectedContent + '\nhello')
+})
