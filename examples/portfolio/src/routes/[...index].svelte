2a7f79155ee93e46696f8b56a37762525add66dc
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-01-23 17:39:12 +0100

    fix: breaking change in sveltekit examples

--- examples/portfolio/src/routes/[...index].svelte
+++ examples/portfolio/src/routes/[...index].svelte
@@ -6,7 +6,7 @@
 
     /* for SSR we need to tell Sveltekit to wait for
        Routify to finish loading its components */
-    export const load = ({ url }) => router.url.replace(url.path)
+    export const load = ({ url }) => router.url.replace(url.pathname)
 </script>
 
 <Router {router} />
--- examples/sveltekit/src/routes/[...index].svelte
+++ examples/sveltekit/src/routes/[...index].svelte
@@ -4,8 +4,9 @@
 
     const router = createRouter({ routes })
 
-    // for SSR we need to tell Sveltekit to wait for Routify to finish loading its components
-    export const load = ({ url }) => router.url.replace(url.path)
+    /* for SSR we need to tell Sveltekit to wait for
+       Routify to finish loading its components */
+    export const load = ({ url }) => router.url.replace(url.pathname)
 </script>
 
 <Router {router} />
