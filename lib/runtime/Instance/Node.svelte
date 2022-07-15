3a8be92dddbee590b1ef293234c65f610e188d18
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-07-15 18:53:24 +0200

    refactor: components were moved to node.module.default

--- lib/runtime/Instance/Node.svelte
+++ lib/runtime/Instance/Node.svelte
@@ -7,11 +7,12 @@
     const context = { ...getContext(CTX), node }
     setContext(CTX, context)
 
-    let Component = node.module
-    if (!Component && node.asyncModule) node.asyncModule().then(r => (Component = r))
+    let Component = node.module?.default
+    if (!Component && node.asyncModule)
+        node.asyncModule().then(r => (Component = r.default))
 </script>
 
-{#if node.module}
+{#if node.module?.default}
     {#if Component}
         <Component {...passthrough} {context}>
             <slot />
