6aa1e6f7f975e2d9f9b2e0358ac8dcedc59025da
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-08-02 22:14:06 +0200

    feat: create default 404 page

---
+++ lib/buildtime/plugins/filemapper/lib/utils/[...404].svelte
@@ -0,0 +1,16 @@
+<script context="module">
+    export const load = ({ route }) => ({
+        status: 404,
+        error: '[Routify] Page could not be found.',
+        props: { url: route.url },
+    })
+</script>
+
+<script>
+    export let url
+</script>
+
+<!-- This file was created by Routify.
+     To customize the error page, create a catchall page 
+     in the root of your project. Eg. [...404].svelte -->
+404 - Could not find the page "{url}"
