3fe499f76ece4981c5afdd06d038c395a571175b
VictorCam <43551031+VictorCam@users.noreply.github.com>
2022-08-19 21:21:29 -0500

    update: fixed edge case on root warnings
    I believe I have it 100% working now. I think there was a strange edge case that maybe had to do with the _layout. I tested it out with the template https://example.routify.dev/ and now the root component in development will not give out the warnings and it will not appear in development.

--- runtime/utils/index.js
+++ runtime/utils/index.js
@@ -64,7 +64,7 @@ export function suppressComponentWarnings(ctx, tick) {
   
   const name = ctx.componentFile.name
     .replace(/Proxy<_?(.+)>/, '$1') //nollup wraps names in Proxy<...>
-    .replace(/^Index$/, (sPath.length == 0 && process.env.NODE_ENV != 'production') ? ctx.component.parent.absolutePath.split('/').pop() : sPath.split('/').pop()) //nollup names Index.svelte index. We want a real name
+    .replace(/^Index$/, (sPath.length == 0 && process.env.NODE_ENV != 'production') ? ctx.component.importPath.split('/').slice(-2, -1)[0] : sPath.split('/').pop()) //nollup names Index.svelte index. We want a real name
     .replace(/^./, s => s.toUpperCase()) //capitalize first letter
     .replace(/\:(.+)/, 'U5B$1u5D') // :id => U5Bidu5D
 
