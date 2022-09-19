ffaa16ab890b6fc43a0b626de63d8bf752c23f93
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-09-19 13:25:47 +0200

    feat: added load to kitchensink example

---
+++ examples/kitchensink/src/routes/load/index.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+    export const load = async () => {
+        await new Promise(resolve => setTimeout(resolve, 1000))
+        return { props: { date: new Date() } }
+    }
+</script>
+
+<script>
+    export let date
+</script>
+
+Date: {date}
