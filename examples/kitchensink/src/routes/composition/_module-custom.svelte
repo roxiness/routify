267dbb9c8efca036cd32600dd35706da3e194090
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-11-18 14:19:06 +0100

    chore: kitchensink & examples

--- examples/kitchensink/src/routes/composition/_module-custom.svelte
+++ examples/kitchensink/src/routes/composition/_module-custom.svelte
@@ -1,4 +1,16 @@
-<h3>Custom module</h3>
-<div style="border: 4px solid red">
+<div>
+    <span>composition/_module.custom-svelte</span>
     <slot />
 </div>
+
+<style>
+    div {
+        border: 4px solid red;
+        border-top-width: 24px;
+    }
+    div > span {
+        position: absolute;
+        top: -20px;
+        color: white;
+    }
+</style>
