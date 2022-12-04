89b96fe837ffac1a83ca6a644d6dc22cda9cb007
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-12-04 12:34:21 +0100

    refactor: deprecate inlineNav component in favor of slot=multi

--- lib/components/Nested.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import Component from '../runtime/Router/Component.svelte'
-    import { context } from '@roxi/routify'
-    export let fragments
-    fragments = fragments || $context.route.fragments.slice(1)
-</script>
-
-<Component {fragments} />
