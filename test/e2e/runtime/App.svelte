845c11e959cf9e05b4bf246a702d53f42f4817ce
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-12-04 12:31:27 +0100

    test: deprecate old test

--- test/e2e/runtime/App.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import { Routify, Router } from '../../../lib/runtime/index.js'
-    import routes from './.routify/routes.default.js'
-    const instance = new Routify({ routes, debugger: false })
-</script>
-
-<Router {instance} />
--- test/e2e/runtime/routes/_module.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-</script>
-
-<nav>
-    <a href="/">home</a>
-    <a href="/blog">blog</a>
-</nav>
-<slot><!-- optional fallback --></slot>
