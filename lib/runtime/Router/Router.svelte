3568325b88b49f9d107692ffd74e75d4a2934e6a
Jakob Rosenberg <jakobrosenberg@gmail.com>
2023-04-13 16:58:41 +0200

    fix: clickHandler couldn't be configured in router object

--- lib/runtime/Router/Router.svelte
+++ lib/runtime/Router/Router.svelte
@@ -90,7 +90,7 @@
         // todo check that a router hasn't already been added to this element
         elem['__routify_meta'] = { ...elem['__routify_meta'], router: router }
 
-        let clickScopeElem = resolveIfAnonFn(clickHandler?.elem || elem, [elem])
+        let clickScopeElem = resolveIfAnonFn(router.clickHandler?.elem || elem, [elem])
 
         // passthrough should be handled by clickHandler.callback instead?
         if (!router.passthrough) {
@@ -102,7 +102,7 @@
     const handleClick = event => {
         /** @type {string|false}*/
         let url = getUrlFromClick(event)
-        url = clickHandler.callback?.(event, url) ?? url
+        url = router.clickHandler.callback?.(event, url) ?? url
         if (typeof url === 'string') router.url.push(url)
     }
 
