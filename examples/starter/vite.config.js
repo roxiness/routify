90cd06bb76c6f15a15546f025918e084a6c16815
Jakob Rosenberg <jakobrosenberg@gmail.com>
2023-05-10 10:20:28 +0200

    fix: starter template should prerender pages

--- examples/starter/vite.config.js
+++ examples/starter/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
 
     plugins: [
         routify({
-            ssr: { enable: false },
+            ssr: { enable: !!production },
         }),
         svelte({
             compilerOptions: {
