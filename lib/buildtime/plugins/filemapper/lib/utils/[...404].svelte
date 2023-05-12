a976b74f9dfa4372ddfa5186defcbaf9adade808
Jakob Rosenberg <jakobrosenberg@gmail.com>
2023-05-12 18:00:03 +0200

    fix: make 404 page more helpful

--- lib/buildtime/plugins/filemapper/lib/utils/[...404].svelte
+++ lib/buildtime/plugins/filemapper/lib/utils/[...404].svelte
@@ -4,6 +4,8 @@
         error: '[Routify] Page could not be found.',
         props: { url: route.url },
     })
+
+    const isDev = import.meta.env?.DEV
 </script>
 
 <script>
@@ -13,4 +15,52 @@
 <!-- This file was created by Routify.
      To customize the error page, create a catchall page 
      in the root of your project. Eg. [...404].svelte -->
-404 - Could not find the page "{url}"
+<div class="four04">
+    <h1>404 - Page Not Found</h1>
+    <p>
+        The page <code>{url}</code> could not be found. Please check the URL or go back to
+        the <a href="/">homepage</a>.
+    </p>
+    {#if isDev}
+        <div>
+            <h5>Dev note:</h5>
+            <ul>
+                <li>
+                    To customize this page, create a
+                    <code>[...404].svelte</code> file in the root of your project.
+                </li>
+                <li>
+                    You can copy the necessary file from <code
+                        >.routify/components/[...404].svelte</code
+                    >.
+                </li>
+                <li>
+                    Custom 404 files can be created at any level of your project. For
+                    example, in <code>src/pages/blog/[...404].svelte</code>.
+                </li>
+            </ul>
+        </div>
+    {/if}
+</div>
+
+<style>
+    div {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        text-align: center;
+    }
+    div.four04 > * {
+        margin-top: 1em;
+    }
+
+    ul {
+        padding: 0;
+    }
+    ul li {
+        opacity: 75%;
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+    }
+</style>
