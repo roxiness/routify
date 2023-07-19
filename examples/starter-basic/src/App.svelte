64e70582c50abc7b9069d03aecde8da7f3b9cf39
Jakob Rosenberg <jakobrosenberg@gmail.com>
2023-07-19 18:41:53 +0200

    fix: get rid of warning in starter-basic example

--- examples/starter-basic/src/App.svelte
+++ examples/starter-basic/src/App.svelte
@@ -1,10 +1,8 @@
 <script context="module">
     import { Router, createRouter } from '@roxi/routify'
     import routes from '../.routify/routes.default.js'
-    import ProgressBar from './components/ProgressBar.svelte'
 
     export const router = createRouter({ routes })
 </script>
 
 <Router {router} />
-<ProgressBar {router} />
