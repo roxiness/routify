50e97ff6a5f0921ee2f69fd2671fcaf339eba447
Jakob Rosenberg <jakobrosenberg@gmail.com>
2023-07-24 10:37:05 +0200

    feat: made express starter more flexible

--- examples/express/server.js
+++ examples/express/server.js
@@ -1,24 +1,30 @@
-import express from "express";
-import { createServer as createViteServer } from "vite";
-import { routifyViteSsr, routifyProdSsr } from "@roxi/routify/lib/extra/express-plugin/index.js";
+import express from 'express'
+import { createServer as createViteServer } from 'vite'
+import {
+    routifyViteSsr,
+    routifyProdSsr,
+} from '@roxi/routify/lib/extra/express-plugin/index.js'
 
 async function createServer() {
-  const app = express();
+    const app = express()
 
-  // example endpoint
-  app.get("/api", (req, res) => {
-    res.json({ hello: "world" });
-  });
+    // example endpoint
+    app.get('/api', (req, res) => {
+        res.json({ hello: 'world' })
+    })
 
-  if (process.env.NODE_ENV === "development") {
-    app.use(await routifyViteSsr(createViteServer));
-  } else {
-    app.use(express.static("dist/client"));
-    app.use(await routifyProdSsr('dist/server/serve.js'));
-  }
+    if (process.env.NODE_ENV === 'development') {
+        const options = {
+            // viteServerConfig: { root: 'frontend' } // optional location for vite.config.js
+        }
+        app.use(await routifyViteSsr(createViteServer, options))
+    } else {
+        app.use(express.static('dist/client'))
+        app.use(await routifyProdSsr('dist/server/serve.js'))
+    }
 
-  app.listen(3000);
-  console.log("listening on http://localhost:3000");
+    app.listen(3000)
+    console.log('listening on http://localhost:3000')
 }
 
-createServer();
+createServer()
