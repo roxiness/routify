d1265b360885ecb10ed3063732f02b6230a965dc
Jakob Rosenberg <jakobrosenberg@gmail.com>
2023-07-30 20:45:39 +0200

    chore: update manifest.js for starter-basic

--- examples/starter-basic/manifest.js
+++ examples/starter-basic/manifest.js
@@ -1,4 +1,39 @@
+import { writeFile, rm } from 'fs/promises'
+
 export default {
     name: 'starter-basic',
     description: 'A basic Routify starter template',
+    test: {
+        tests: [{ page: '/', contains: 'Welcome to Your New Routify 3 Project!' }],
+    },
+    features: [
+        {
+            label: 'examples',
+            value: 'examples',
+            hint: 'Add examples',
+            initial: true,
+        },
+        {
+            label: 'components',
+            value: 'components',
+            hint: 'Add pre-made components to src/components',
+            initial: true,
+        },
+    ],
+    postInstall: async options => {
+        if (!options.features.includes('examples')) {
+            // write <h1>Hello world</h1> to index.svelte
+            const indexSvelte = `${options.projectDir}/src/routes/index.svelte`
+            await writeFile(
+                indexSvelte,
+                `<h1>Welcome to Your New Routify 3 Project!</h1>`,
+            )
+            // remove examples folder
+            await rm(`${options.projectDir}/src/routes/examples`, { recursive: true })
+        }
+        if (!options.features.includes('components')) {
+            // remove components folder
+            await rm(`${options.projectDir}/src/components`, { recursive: true })
+        }
+    },
 }
