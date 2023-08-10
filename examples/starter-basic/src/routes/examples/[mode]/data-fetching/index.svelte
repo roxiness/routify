33de9bab24a5a43c5215670311d79b4f0656230f
Jakob Rosenberg <jakobrosenberg@gmail.com>
2023-08-10 22:53:36 +0200

    fix: weird paragraph in examples/data-fetching

--- examples/starter-basic/src/routes/examples/[mode]/data-fetching/index.svelte
+++ examples/starter-basic/src/routes/examples/[mode]/data-fetching/index.svelte
@@ -4,11 +4,7 @@
 
 <p>
     This example demonstrates how to utilize the `load` hook to fetch and return data via
-    a
-</p>
-
-<p>
-    "props" object. Navigate to data.svelte to see how we retrieve data from a Star Wars
+    a "props" object. Navigate to data.svelte to see how we retrieve data from a Star Wars
     API and make it accessible for our component.
 </p>
 
