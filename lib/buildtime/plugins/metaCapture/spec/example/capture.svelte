251cd1a49639793a2c55418fe5127a98001b4388
Jakob Rosenberg <jakobrosenberg@gmail.com>
2023-09-04 22:36:39 +0200

    feat: metaCapure plugin

---
+++ lib/buildtime/plugins/metaCapture/spec/example/capture.svelte
@@ -0,0 +1,32 @@
+<!-- routify:meta capture -->
+
+<div class="container">
+    <div class="title">Logical Fallacies in Software Development</div>
+    <div class="content">
+        <p>
+            As we navigate through the intricate world of software development, especially
+            in the realms of JavaScript and JSDoc, it is not uncommon to encounter a range
+            of logical fallacies...
+        </p>
+
+        <h2>Confirmation Bias in Code Review</h2>
+        <p>
+            Explanation and examples on how confirmation bias can sometimes mislead during
+            code reviews, and strategies to mitigate this.
+        </p>
+
+        <h2>Appeal to Authority in Tech Communities</h2>
+        <p>
+            A discussion on the consequences of relying too much on prominent figures'
+            opinions and the importance of individual critical thinking in the tech
+            community.
+        </p>
+
+        <h2>Straw Man Fallacy in Development Debates</h2>
+        <p>
+            An overview of how the straw man fallacy can sometimes divert a meaningful
+            discussion and ways to stay focused on the real issues.
+        </p>
+    </div>
+    <div class="footer">© 2023, A Logical Developer's Blog</div>
+</div>
---
+++ lib/buildtime/plugins/metaCapture/spec/example/captureSegments.svelte
@@ -0,0 +1,34 @@
+<div class="container">
+    <div class="title">Logical Fallacies in Software Development</div>
+    <div class="content">
+        <!-- routify:meta captureStart="intro" -->
+        <p>
+            As we navigate through the intricate world of software development, especially
+            in the realms of JavaScript and JSDoc, it is not uncommon to encounter a range
+            of logical fallacies...
+        </p>
+        <!-- routify:meta captureEnd="intro" -->
+
+        <h2>Confirmation Bias in Code Review</h2>
+        <p>
+            Explanation and examples on how confirmation bias can sometimes mislead during
+            code reviews, and strategies to mitigate this.
+        </p>
+
+        <h2>Appeal to Authority in Tech Communities</h2>
+        <p>
+            A discussion on the consequences of relying too much on prominent figures'
+            opinions and the importance of individual critical thinking in the tech
+            community.
+        </p>
+
+        <!-- routify:meta captureStart="strawman" -->
+        <h2>Straw Man Fallacy in Development Debates</h2>
+        <p>
+            An overview of how the straw man fallacy can sometimes divert a meaningful
+            discussion and ways to stay focused on the real issues.
+        </p>
+        <!-- routify:meta captureEnd="strawman" -->
+    </div>
+    <div class="footer">© 2023, A Logical Developer's Blog</div>
+</div>
