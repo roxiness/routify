4a5d18189215d208c4cf717a842e8c7efeb6a4d6
Jakob Rosenberg <jakobrosenberg@gmail.com>
2023-10-04 11:15:01 +0200

    update Inline-Params description for starter-basic

--- examples/starter-basic/src/routes/examples/[mode]/inline-params/_module.svelte
+++ examples/starter-basic/src/routes/examples/[mode]/inline-params/_module.svelte
@@ -2,6 +2,9 @@
     let cities = ['new-york', 'london', 'tokyo', 'sydney']
 </script>
 
+<!-- routify:_meta _description="<pre><code>let params = {<br>  city = ['new-york', 'london'...]<br>...<br>slot inline={{ params }}</code></pre>   " -->
+<!-- routify:meta _description="Using <code>&lt;slot inline={{params}} /&gt;</code>, we can pass 4 city names. This will create 4 instances of <code>inline-params/[city]</code>." -->
+
 <div class="city-box">
     <slot inline={{ params: { city: [...cities] } }} />
 </div>
