cfc44827234d797c04cda81989e94494122c6da1
Jakob Rosenberg <jakobrosenberg@gmail.com>
2024-05-28 14:11:33 +0200

    chore: simplify mobile starter template

--- examples/starter-basic-mobile/src/routes/2.app/_module.svelte
+++ examples/starter-basic-mobile/src/routes/2.app/_module.svelte
@@ -1,19 +1,12 @@
 <script>
+    import Carousel from '@roxi/routify/Carousel'
     import Navigation from './__components/Navigation.svelte'
-    import { RouteOnScroll } from '@roxi/routify/helpers/RouteOnScroll'
-
-    const routeOnScroll = new RouteOnScroll({
-        scrollEvent: 'scroll', // or "scrollend"
-        threshold: 100, // don't trigger until we're within 100px of the page
-        direction: 'horizontal',
-        strategy: 'withinThreshold',
-    })
 </script>
 
 <div class="app">
-    <main class="scroll-snap" data-routify-scroll-lock use:$routeOnScroll>
+    <Carousel>
         <slot />
-    </main>
+    </Carousel>
     <Navigation />
 </div>
 
