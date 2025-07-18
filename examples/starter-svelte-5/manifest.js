66b1180c0bb687e21feebb09f4a36df480ffe8d0
Jakob Rosenberg <jakobrosenberg@gmail.com>
2025-07-18 11:20:54 +0200

    chore: rename starter-basic-svelte-5 to starter-svelte-5

--- examples/starter-svelte-5/manifest.js
+++ examples/starter-svelte-5/manifest.js
@@ -1,5 +1,5 @@
 export default {
-    name: 'starter-basic-svelte-5',
+    name: 'starter-svelte-5',
     description: 'A basic Routify starter template for Svelte 5',
     test: {
         tests: [{ page: '/', contains: 'Hello World' }],
